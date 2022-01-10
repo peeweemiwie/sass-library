b516310ce92c57c5a9aa126daad4eb6ae26464c5
peeweemiwie <miwayagi@gmail.com>
2022-01-10 12:03:39 -0500

    add menu button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,18 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { MdMenuOpen, MdMenu } from 'react-icons/md';
+// import { MdMenuOpen, MdMenu } from 'react-icons/md';
 import './Header.scss';
 const Header = () => {
-	const [isOpen, setIsOpen] = useState(false);
-	const handleClick = () => {
-		isOpen ? setIsOpen(false) : setIsOpen(true);
-	};
+	// const [isOpen, setIsOpen] = useState(false);
+	// const handleClick = () => {
+	// 	isOpen ? setIsOpen(false) : setIsOpen(true);
+	// };
 	return (
-		<header className='header' data-isOpen={isOpen}>
-			<button className='btn btn-border-primary' onClick={handleClick}>
+		<header className='header'>
+			{/* <button className='btn btn-border-primary' onClick={handleClick}>
 				{isOpen && <MdMenuOpen />}
 				{!isOpen && <MdMenu />}
-			</button>
+			</button> */}
 			<h1 className='h1'>
 				<Link to='/'>Design System</Link>
 			</h1>
